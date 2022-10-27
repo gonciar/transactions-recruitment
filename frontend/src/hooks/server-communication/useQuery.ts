c5c98a4c048eda1ba60e1586ea6e15e63f900a1c
Dominik Gonciarz <dominik.gonciarz@gmail.com>
2022-10-27 13:35:40 +0200

    fix hook

--- frontend/src/hooks/server-communication/useQuery.ts
+++ frontend/src/hooks/server-communication/useQuery.ts
@@ -9,26 +9,25 @@ interface UseQueryProps {
 export function useQuery<T>({ url }: UseQueryProps) {
   const [data, setData] = useState<RequestResult<T>>({ status: 'init' });
 
-  async function fetchData(url: string) {
-    setData({ status: 'loading' });
-    try {
-      const data = await fetchTS<T>(url);
-      console.log(data);
-      setData({ status: 'loaded', payload: data });
-    } catch (error) {
-      if (error instanceof Response) {
-        const err = new Error(`Unknown server error occurred: ${error.statusText}`);
-        setData({ status: 'error', error: err });
-      }
-
-      const err = new Error(`Something went wrong`);
-      setData({ status: 'error', error: err });
-    }
-  }
-
   useEffect(() => {
-    fetchData(url);
-  }, [url]);
+    async function fetchData() {
+      setData({ status: 'loading' });
+      try {
+        const data = await fetchTS<T>(url);
+        setData({ status: 'loaded', payload: data });
+      } catch (error) {
+        if (error instanceof Response) {
+          const err = new Error(`Unknown server error occurred: ${error.statusText}`);
+          setData({ status: 'error', error: err });
+        } else {
+          const err = new Error(`Something went wrong`);
+          setData({ status: 'error', error: err });
+        }
+      }
+    }
+
+    fetchData();
+  }, [setData, url]);
 
   return data;
 }
